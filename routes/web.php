9d4c040d0ec9f6d9bca8e317eee472dd1f74ec63
AdrianVyne <imadrian1234@gmail.com>
2023-03-11 15:47:29 +0800

    added route to /testdb

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testdb', function () {
+    try {
+        $results = DB::select('SHOW TABLES');
+        print_r($results);
+    } catch (\Exception $e) {
+        die("Could not connect to the database. " . $e->getMessage());
+    }
+});
