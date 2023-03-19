b7effe3d09430003551ea22da7c698cdb70c17fa
Aki <vinceaki11@gmail.com>
2023-03-19 14:52:16 +0800

    log in page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,22 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    @include("layouts/head")
-    <title>Document</title>
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+  <title>FITWORX LOGIN PAGE</title>
 </head>
-
 <body>
-@include("layouts/navbar-user")
-<h1>Login</h1>
-<form action="/login" method="POST">
-    @csrf
-    <label>Login Name:</label>
-    <input type="text" name="login_name"></input><br>
-    <label>Password:</label>
-    <input type="password" name="password"></input><br>
-    <button type="submit">Login</button>
-</form>
+    <section>
+        <div class="form-box">
+            <div class="form-value">
+                <form action="">
+                    <h2>Login</h2>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="email" required>
+                        <label for="">Email</label>
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" required>
+                        <label for="">Password</label>
+                    </div>
+                    <div class="forget">
+                        <label for=""><input type="checkbox">Remember Me  <a href="#">Forget Password</a></label>
+                      
+                    </div>
+                    <button>Log in</button>
+                    
+                </form>
+            </div>
+        </div>
+    </section>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    
 </body>
-
 </html>
